2ec0203c2fdf79a111c3810caa6afe8ffd950d7a
Lê Đình Dũng <79614212+dungle03@users.noreply.github.com>
2025-07-17 02:10:09 +0700

    Update delete_header.js

--- delete_header.js
+++ delete_header.js
@@ -1,1 +1,24 @@
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+// Hàm set giá trị cho header, hỗ trợ case-insensitive
+function setHeaderValue(headers, key, value) {
+  const lowerKey = key.toLowerCase();
+  // Tìm key thực tế trong headers (case-insensitive)
+  const actualKey = Object.keys(headers).find(k => k.toLowerCase() === lowerKey);
+  if (actualKey) {
+    headers[actualKey] = value;
+  } else {
+    headers[key] = value; // Nếu không tồn tại, thêm mới
+  }
+}
+
+const modifiedHeaders = $request.headers;
+
+// Danh sách headers cần modify (dễ mở rộng)
+const headersToModify = [
+  { key: 'X-RevenueCat-ETag', value: '' } // Set rỗng để xóa
+];
+
+headersToModify.forEach(({ key, value }) => {
+  setHeaderValue(modifiedHeaders, key, value);
+});
+
+$done({ headers: modifiedHeaders });
